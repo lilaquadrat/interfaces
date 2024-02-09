4ca13628661e1c92594e65c18c48563cec52bb41
mschuebel <m.schuebel@lila2.de>
2024-02-09 10:53:21 +0100

    fix(genericdata): fixed genericData data

--- src/GenericData.ts
+++ src/GenericData.ts
@@ -1,16 +1,14 @@
 import { BasicData } from "./BasicData";
 import { Content } from "./Content"
-import { Customers } from "./Customers";
-import { List } from "./List";
-import { Media } from "./Media";
 
 type CompatibleGenericDataType = 'editor' | 'lists' | 'customers' | 'media';
 
 type GenericData = {
     [key in CompatibleGenericDataType]: string[]
 } & {
-    data: Record<string, BasicData<Content | List | Customers | Media>>
+    data: BasicData<Content>[]
 }
+
 
 export {
     GenericData,
