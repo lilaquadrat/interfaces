5abadb6c3fc369dad454a824aeba64d65cce5b3b
mschuebel <m.schuebel@lila2.de>
2025-02-02 18:04:06 +0100

    fix(exportdata): made keys optional

--- src/ExportData.ts
+++ src/ExportData.ts
@@ -2,14 +2,14 @@ export interface ExportData {
   company: string;
   project: string;
   created: number;
-  files: string[];
-  filesize: number;
-  path: string;
-  metadata: {
-    createdAt: string;
-    company: string;
-    project: string;
-    selectedServices: string[];
-    statistics: Record<string, number>;
+  files?: string[];
+  filesize?: number;
+  path?: string;
+  metadata?: {
+    createdAt?: string;
+    company?: string;
+    project?: string;
+    selectedServices?: string[];
+    statistics?: Record<string, number>;
   };
 }
