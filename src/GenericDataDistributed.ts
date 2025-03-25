73d8b3770f11a5892ff32e34d5b877d4e360f4ef
mschuebel <m.schuebel@lila2.de>
2025-03-25 13:56:35 +0100

    fix(genericdatadistributed): added structure

--- src/GenericDataDistributed.ts
+++ src/GenericDataDistributed.ts
@@ -4,11 +4,12 @@ import { Customers } from "./Customers";
 import { CompatibleGenericDataType } from "./GenericData";
 import { List } from "./List";
 import { Media } from "./Media";
+import { Structure } from "./Structure";
 
 type GenericDataDistributed =  {
-    [key in CompatibleGenericDataType]: (BasicData<List>['_id']|BasicData<Content>['_id']|BasicData<Customers>['_id'])[]
+    [key in CompatibleGenericDataType]: (BasicData<List>['_id']|BasicData<Content>['_id']|BasicData<Customers>['_id']|BasicData<Structure>['_id'])[]
 } & {
-    data: Record<string, BasicData<Content | List | Customers | Media>>;
+    data: Record<string, BasicData<Content | List | Customers | Media | Structure>>;
 }
 
 export { GenericDataDistributed };
