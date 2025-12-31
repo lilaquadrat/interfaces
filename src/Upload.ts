c361c03ec6bf3060db73daaf3549c58181912d26
mschuebel <m.schuebel@lila2.de>
2025-12-31 11:42:55 +0100

    fix(upload): make 'filename', 'app', and 'mimetype' properties required in Upload interface

--- src/Upload.ts
+++ src/Upload.ts
@@ -3,15 +3,15 @@ import { ObjectIdString } from './ObjectIdString';
 
 export interface Upload {
     _id?: ObjectId;
-    filename?: string;
+    filename: string;
     prefix?: string;
     company?: string;
     project?: string;
     customer?: ObjectIdString | ObjectId;
     list?: ObjectIdString | ObjectId;
     user?: string;
-    app?: string;
-    mimetype?: string;
+    app: string;
+    mimetype: string;
     size?: number;
     chunks: number;
     paths?: { path: string, index: number }[];
