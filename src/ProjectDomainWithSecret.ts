617e7aecc2efafd10227902754f5b32c18b8a5a7
mschuebel <m.schuebel@lila2.de>
2026-01-03 16:46:53 +0100

    fix(ProjectDomain): rename interface to ProjectDomainWithSecret for clarity

--- src/ProjectDomainWithSecret.ts
+++ src/ProjectDomainWithSecret.ts
@@ -1,7 +1,7 @@
 import { Domain } from "./Domain";
 import { Secret } from "./Secret";
 
-export interface ProjectDomain extends Omit<Domain, 'secret'> {
+export interface ProjectDomainWithSecret extends Omit<Domain, 'secret'> {
   company: string
   project: string
   secret: Secret
